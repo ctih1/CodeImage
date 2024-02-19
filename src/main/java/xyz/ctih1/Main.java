64b7e6fdedaf5c90722369e246c326f297914b09
ctih <78687256+ctih1@users.noreply.github.com>
2024-02-19 19:03:06 +0200

    Updated logging. (#1)
    Co-authored-by: ctih <nevalaonni@proton.me>

--- src/main/java/xyz/ctih1/Main.java
+++ src/main/java/xyz/ctih1/Main.java
@@ -11,12 +11,16 @@ public class Main {
     static List<String> lines;
     static List<String> orderedLines;
     static BufferedImage lastImage;
+    public static int current;
+    public static int total;
+    public static long startTime;
     public static void main(String[] args) throws IOException, RateLimitException {
         if(args.length<5) {
             System.err.println("Invalid usage. java -jar <username> <reponame> <branch> <.filetype> <token> <(optional) type: maxHeight, minHeight");
             return;
         }
-
+        System.out.println("[Main] starting...");
+        startTime = System.currentTimeMillis();
         Image image = new Image();
 
         Code code = new Code();
@@ -24,9 +28,11 @@ public class Main {
         lines = code.get(args[0],args[1],args[2],args[3],args[4]);
         orderedLines = (lines.stream().sorted(Comparator.comparingInt(String::length)).collect(Collectors.toList()));
         if(args.length>=6&&args[5].equalsIgnoreCase("maxHeight")) {
+            System.out.println("[Main] Reversing list...");
             Collections.reverse(orderedLines);;
         }
-
+        total = lines.size();
+        current = 0;
         for(String line : orderedLines) {
             lastImage = image.create(line,lastImage);
         }
